7caaad06f3b380dd8f4745ff3cb55b210ee192c2
Aryan <126584072+AryanElahi@users.noreply.github.com>
2024-03-25 06:42:49 +0330

    the foundation of authentication has been added to a temparary file named authcode. the functions will go to service after completing

---
+++ src/authcode.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const morgan = require("morgan")
+const creatErrors = require("http-errors")
+//const env = require("dotenv").config()
+
+const app = express()
+
+app.get("/", async (req, res, next) => {
+    res.send("hello world again")
+})
+app.use(async(req, res, next) => {
+  //  const error = new Error('not found')
+    //error.status = 404 
+    //next (error)
+    next(creatErrors.NotFound())
+})
+app.use( (err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send({
+        erorr :{
+            status : err.status || 500,
+            message: err.message,
+        }
+    })
+})
+const port = 3000
+
+app.listen(port, () => {
+    console.log ("server is runnng")
+})
