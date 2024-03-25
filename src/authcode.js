156f2212e4b3de331fef693278c20eac539a37a0
Aryan <126584072+AryanElahi@users.noreply.github.com>
2024-03-26 02:19:41 +0330

    running functions has been added to authcode and functionality and routes has been added to auth.rout. all extra routes removed and creating new user works properlly in rout file with user name and phone checker. validation method is not defiened and a simple error handeling has been implemented in both main files

--- src/authcode.js
+++ src/authcode.js
@@ -2,9 +2,14 @@ const express = require("express")
 const morgan = require("morgan")
 const creatErrors = require("http-errors")
 const AuthRouts = require("./api/routes/V1/auth.rout")
+
 require("dotenv").config()
 
 const app = express()
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 
 app.get("/", async (req, res, next) => {
     res.send("hello world again")
@@ -30,5 +35,5 @@ app.use( async (err, req, res, next) => {
 const port = 3000
 
 app.listen(port, () => {
-    console.log ("server is runnng")
+   console.log ("server is runnng")
 })
