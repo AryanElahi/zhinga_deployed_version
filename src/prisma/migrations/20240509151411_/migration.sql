c1dab198dc9ea3767e413984fe7aa6966a0324b7
Aryan <126584072+AryanElahi@users.noreply.github.com>
2024-05-09 18:45:09 +0330

    check rol has been added to database

---
+++ src/prisma/migrations/20240509151411_/migration.sql
@@ -0,0 +1,29 @@
+-- AlterTable
+ALTER TABLE "AgricultureLand" ADD COLUMN     "check" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "Apartement" ADD COLUMN     "check" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "Enterprise" ADD COLUMN     "check" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "Garden" ADD COLUMN     "check" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "House" ADD COLUMN     "check" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "Land" ADD COLUMN     "check" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "Nitche" ADD COLUMN     "check" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "ServiceLand" ADD COLUMN     "check" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "Shop" ADD COLUMN     "check" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "admin" BOOLEAN NOT NULL DEFAULT false;
