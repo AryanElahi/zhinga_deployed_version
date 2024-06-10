cf4c71e629e612d44108fc4b1788234d9ef600d5
Aryan <126584072+AryanElahi@users.noreply.github.com>
2024-06-10 17:12:25 +0330

    clean the admin panel services

--- src/api/routes/V1/admin/admin.rout.js
+++ src/api/routes/V1/admin/admin.rout.js
@@ -36,57 +36,11 @@ try {
         "unchecked requests" : uncheckedRequest.number.toString(),
         "all visitors" : allVisitors.number.toString(),
         "daily visitors" : dailyVisitors.toString()
-        //"unchecked requests" : 
     })
 } catch (error) {
     if (error.isJoi === true) error.status = 422
     next(error)
 }
-})
-router.post("/login", async (req, res, next) => {
-    try {
-        const result = await loginVal.validateAsync(req.body) 
-        const user = await getUserByPhone(result.phone)
-        const compare = await isValid(result.password, user.password)
-        if (!user || result.softDelete) throw creatErrors.NotFound("user is not regesterd")
-        if (compare === false) throw creatErrors.Unauthorized("username of password is not correct")
-        const refreshToken = await signRefreshToken(user.phone)
-        const AccessToken = await signAccessToken(user.phone)
-        res.send({refreshToken, AccessToken})
-    } catch (error) {
-        if (error.isJoi === true) error.status = 422
-        if (error.isJoi === true) return next(creatErrors.BadRequest("username or password is invalid"))
-        next(error)
-    }
-})
-
-router.post("/refreshToken", async (req, res, next) => {
-    try {
-        const {refreshToken} = req.body
-        if (!refreshToken) throw creatErrors.BadRequest()
-        const phone = await verifyRefreshToken(refreshToken)
-        const AccessToken = await signAccessToken(phone)
-        const RefreshToken = await signRefreshToken(phone)
-        res.send ({AccessToken, RefreshToken})
-    } catch (error) {
-        if (error) throw error
-    }
-})
-
-router.put ("/updateuser", async (req, res) => {
-    try {
-        let result = await signupVal.validateAsync(req.body)
-        if (!req.headers["authorization"]) next (creatErrors.Unauthorized())
-        const authheader = req.headers["authorization"]
-        let phone = await getUserByAccessToken(authheader)
-        res.send(await updateUser(phone, result))
-    } catch (error) {
-        if (error) throw error
-    }
-
-})
-router.delete("/logout", async (req, res, next) => {
-    res.send("logout")
 })
 
 module.exports = router
