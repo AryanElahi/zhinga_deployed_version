ed04549bd5da5e50db7ff700807dc405f907db78
Aryan <126584072+AryanElahi@users.noreply.github.com>
2024-10-29 10:17:12 +0330

    user creat announcement has been updated

---
+++ src/prisma/migrations/20241029064618_final/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `softDelete` on the `property` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "property" DROP COLUMN "softDelete";
