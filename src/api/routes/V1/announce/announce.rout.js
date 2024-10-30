68a78adc1e6804d771fa8b4c09f6a2ebf1268089
Aryan <126584072+AryanElahi@users.noreply.github.com>
2024-10-31 01:01:51 +0330

    new error handeling system has been added to announcement routs

--- src/api/routes/V1/announce/announce.rout.js
+++ src/api/routes/V1/announce/announce.rout.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const creatErrors = require("http-errors")
+const createError = require("http-errors")
 const {creat, update} = require("../../../../validation/announce.crud.validation")
 const { 
     creatAnnouncement,
@@ -16,72 +16,105 @@ const {getUserByAccessToken} = require ("./../../../../services/user/auth")
 const {verifyAccessToken} = require("../../../middlewares/isAuth.middleware")
 
 router.post ("/creatAnnounce", verifyAccessToken , async (req, res, next) => {
-    let result = await creat.validateAsync(req.body)
-    const authheader = req.headers["authorization"]
-    const bearertoken = authheader.split(' ')
-    console.log(bearertoken)
-    const token = bearertoken[1]
-    console.log(token)
-    const userId = await getUserByAccessToken(token)
-    result.Uid = String(new Date().getTime()) 
-    console.log (result)
-    const  newA = await creatAnnouncement(result, userId)
-    res.send (newA)
+    try {
+        let result = await creat.validateAsync(req.body)
+        const authheader = req.headers["authorization"]
+        const bearertoken = authheader.split(' ')
+        console.log(bearertoken)
+        const token = bearertoken[1]
+        console.log(token)
+        const userId = await getUserByAccessToken(token)
+        result.Uid = String(new Date().getTime()) 
+        console.log (result)
+        const  newA = await creatAnnouncement(result, userId)
+        res.send (newA)        
+    } catch (error) {
+        next(createError(500, "An unexpected error occurred"));
+    }
 })
-router.post("/uploadPhotos",verifyAccessToken , async (req, res) => {
-    upload(req, res, async (err) => { 
-        if (err) {
-            return res.status(400).json({
-                success: 0,
-                message: err.message
-            });
-        }
-        if (!req.files || req.files.length === 0) {
-            return res.status(400).json({
-                success: 0,
-                message: 'file doesnt exist'
-            });
-        }
-        const imageUrls = req.files.map(file => `http://localhost:3000/photos/${file.filename}`);
-        try {
-            const adding = await photo_adding(req.body.Uid, imageUrls)
-            res.status(200).json({
-                success: 1,
-                message: "success",
-                files: imageUrls
-            });
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({
-                success: 0,
-                message: "error while uploading"
-            });
-        }
-    });
+router.post("/uploadPhotos",verifyAccessToken , async (req, res, next) => {
+    try {
+        upload(req, res, async (err) => { 
+            if (err) {
+                return res.status(400).json({
+                    success: 0,
+                    message: err.message
+                });
+            }
+            if (!req.files || req.files.length === 0) {
+                return res.status(400).json({
+                    success: 0,
+                    message: 'file doesnt exist'
+                });
+            }
+            const imageUrls = req.files.map(file => `http://localhost:3000/photos/${file.filename}`);
+            try {
+                const adding = await photo_adding(req.body.Uid, imageUrls)
+                res.status(200).json({
+                    success: 1,
+                    message: "success",
+                    files: imageUrls
+                });
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({
+                    success: 0,
+                    message: "error while uploading"
+                });
+            }
+        });        
+    } catch (error) {
+        next(createError(500, "An unexpected error occurred"));
+    }
 });
-router.post("/getbystatecode",verifyAccessToken , async (req, res) => {
-    const state = req.body.state_code
-    res.send(await getByStateCode(state))
+router.post("/getbystatecode",verifyAccessToken , async (req, res, next) => {
+    try {
+        const state = req.body.state_code
+        res.send(await getByStateCode(state))        
+    } catch (error) {
+        next(createError(500, "An unexpected error occurred"));
+    }
 })
-router.get("/getallannounce",verifyAccessToken , async (req, res) => {
-    res.send(await getAllAnnouns())
+router.get("/getallannounce",verifyAccessToken , async (req, res, next) => {
+    try {
+        res.send(await getAllAnnouns())        
+    } catch (error) {
+        next(createError(500, "An unexpected error occurred"));
+    }
 })
-router.post("/getbyUid",verifyAccessToken , async (req, res) => {
-    const Uid = req.body.Uid
-    res.send(await getByUid(Uid))
+router.post("/getbyUid",verifyAccessToken , async (req, res, next) => {
+    try {
+        const Uid = req.body.Uid
+        res.send(await getByUid(Uid))        
+    } catch (error) {
+        next(createError(500, "An unexpected error occurred"));
+    }
 })
-router.put("/updateannoun",verifyAccessToken , async (req, res) => {
-    let result = await update.validateAsync(req.body)
-    res.send (await updateAnnoun(result))
+router.put("/updateannoun",verifyAccessToken , async (req, res, next) => {
+    try {
+        let result = await update.validateAsync(req.body)
+        res.send (await updateAnnoun(result))
+    } catch (error) {
+        next(createError(500, "An unexpected error occurred"));
+    }
+
 })
-router.post("/search" ,verifyAccessToken , async (req, res) => {
-    console.log(req.body)
-    const result = await search(req.body)
-    res.send(result)
+router.post("/search" ,verifyAccessToken , async (req, res, next) => {
+    try {
+        console.log(req.body)
+        const result = await search(req.body)
+        res.send(result)        
+    } catch (error) {
+        next(createError(500, "An unexpected error occurred"));
+    }
 })
-router.delete("/hdelete",verifyAccessToken , async (req, res) => {
-    const Uid = req.body.Uid
-    res.send (await deleteAnnoun(Uid))
+router.delete("/hdelete",verifyAccessToken , async (req, res, next) => {
+    try {
+        const Uid = req.body.Uid
+        res.send (await deleteAnnoun(Uid))        
+    } catch (error) {
+        next(createError(500, "An unexpected error occurred"));
+    }
 })
 
 module.exports = router
