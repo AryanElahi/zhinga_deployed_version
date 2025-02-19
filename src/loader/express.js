9e976c37418fc006f30cada84eba8285872c434b
Aryan <126584072+AryanElahi@users.noreply.github.com>
2025-02-19 20:27:33 +0330

    cors madule has been added to express file

--- src/loader/express.js
+++ src/loader/express.js
@@ -1,38 +1,40 @@
-const express = require("express")
-const morgan = require("morgan")
-const createErrors = require("http-errors")
-const routes = require("../api/routes")
-require("dotenv").config()
-const {save_visitor} = require("./../services/adminpanel/visitCountingServices")
-const { errorHandler, notFoundHandler } = require("./..//api/middlewares/errorHandeler");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");  // اضافه کردن cors
+const createErrors = require("http-errors");
+const routes = require("../api/routes");
+require("dotenv").config();
+const { save_visitor } = require("./../services/adminpanel/visitCountingServices");
+const { errorHandler, notFoundHandler } = require("./../api/middlewares/errorHandeler");
 
 const expressLoader = async (app) => {
-    app.use(morgan("dev"))
-    app.use(express.json())
-    app.use(express.urlencoded({extended: true}))
+    // تنظیمات CORS
+    const corsOptions = {
+        origin: 'http://your-frontend-url.com',  // آدرس فرانت‌اند خود را اینجا قرار بده
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],  // متدهای مجاز
+        allowedHeaders: ['Content-Type', 'Authorization'],  // هدرهای مجاز
+        credentials: true,  // اگر نیاز به ارسال کوکی یا اعتبار سنجی است
+    };
+
+    // اضافه کردن CORS به اپلیکیشن
+    app.use(cors(corsOptions));
+
+    app.use(morgan("dev"));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
     app.get("/", async (req, res, next) => {
-        await save_visitor(req)
-        res.send("Home Rout")
-    })
-    app.use("/api", routes())
+        await save_visitor(req);
+        res.send("Home Rout");
+    });
+
+    app.use("/api", routes());
     app.use(notFoundHandler);
     app.use(errorHandler);
-//    app.use(async(req, res, next) => {
-//        next(createErrors.NotFound())
-//        })
-//    app.use( async (err, req, res, next) => {
-//    res.status(err.status || 500)
-//    res.send({
-//        erorr :{
-//            status : err.status || 500,
-//            message: err.message,
-//        }
-//    })
-// })
 
-app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
-})
-}
+    app.listen(3000, () => {
+        console.log(`Server is running on port 3000`);
+    });
+};
 
 module.exports = expressLoader;
